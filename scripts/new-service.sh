adf8bcf9af15ad16e7570eaacdd207253455c153
Alex Humphreys <alex.humphreys@pleo.io>
2024-03-23 00:43:02 +0100

    Tried to get github reuseable workflows going?

--- scripts/new-service.sh
+++ scripts/new-service.sh
@@ -3,6 +3,8 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+GITHUB_ORG="this-org"
+GITHUB_REPO="this-repo"
 SERVICE_DIR_NAME="services"
 # --> LOCO_TEMPLATE= {lightweight-service|rest-api|saas}
 export LOCO_APP_NAME="$1"
@@ -12,5 +14,4 @@ loco new -p "./$SERVICE_DIR_NAME"
 dasel put -f Cargo.toml -r toml -t string -v "$SERVICE_DIR_NAME/$LOCO_APP_NAME" '.workspace.members.append()'
 dasel delete -f "./$NEW_SERVICE_DIR/Cargo.toml" workspace
 
-# TODO point reusable workflow to the new service
-# https://docs.github.com/en/actions/using-workflows/reusing-workflows#creating-a-reusable-workflow
+dasel put -r yaml -f .github/workflows/reuse-ci.yml -t string -v "$GITHUB_ORG/$GITHUB_REPO/$NEW_SERVICE_DIR/.github/workflows" '.jobs.new.uses'
